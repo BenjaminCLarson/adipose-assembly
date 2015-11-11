65bee8e3e4978110ac194f9e156c2c9594f1cf68
benjaminclarson <benjaminclarson@gmail.com>
2015-11-11 13:11:15 -0500

    hey ben you suck

--- DeckOCards.py
+++ DeckOCards.py
@@ -2,46 +2,40 @@ __author__ = 'blarson2'
 
 # !/usr/local/bin/python
 
-import random
+from random import *
 import datetime
 
-def translate(deck) -> list:
-    hand = ['north has the ', 'east has the ', 'south has the ', 'west has the ']
-    minor = ['ace ', 'deuce ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ',
-             'nine ', 'ten ', 'page ', 'knight ', 'queen ', 'king ']
-    suit = ['of wands.', 'of cups.', 'of swords.', 'of coins.', '.']
+def shuff(deck):
+    temp = []
+    for i in deck:
+        popped_elem = deck.index(choice(deck))
+        temp.append(deck.pop(popped_elem))
 
-    major = ['magician.', 'priestess.', 'empress.', 'emperor.', 'hierophant.', 'lovers.', 'chariot.',
-             '\b\b\b\bstrength.', 'hermit.', '\b\b\b\bfortune.', '\b\b\b\bjustice', 'hanged man.',
-             '\b\b\b\bdeath.', '\b\b\b\btemperance.', 'devil.', 'tower.', 'star.', 'moon.', 'sun.',
-             '\b\b\b\bjudgement.', 'world.', 'fool.']
-
-    table = []
-
-    for i in range(78):
-        table.append(hand[i % 4])
-        if deck[i] < 56:
-            table[i] += minor[deck[i % 14]]
-            table[i] += suit[deck[i // 14]]
-        else:
-            table[i] += major[deck[i % 56]]
-            table[i] += suit[4]
-
-    for i in table:
-        print(table[i])
-    return table
-
+    return temp
 
 def main():
-    deck = []
-    for i in range(78):
-        deck.append(0)
-        deck[i] = i
-        print(deck[i])
+    minor = ['ace ', 'deuce ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ',
+             'nine ', 'ten ', 'page ', 'knight ', 'queen ', 'king ']
 
-    random.seed(datetime)
-    random.shuffle(deck)
-    table = translate(deck)
+    suit = ['of wands.', 'of cups.', 'of swords.', 'of coins.']
+
+    major = ['magician.', 'priestess.', 'empress.', 'emperor.', 'hierophant.', 'lovers.', 'chariot.',
+             '\b\b\b\bstrength.', 'hermit.', '\b\b\b\bfortune.', '\b\b\b\blust.', '\b\b\b\bjustice', 'hanged man.',
+             '\b\b\b\bdeath.', '\b\b\b\btemperance.', 'devil.', 'tower.', 'star.', 'moon.', 'sun.',
+             '\b\b\b\bjudgement.', 'world.', 'universe.', 'fool.']
+
+    deck = []
+
+    for i in range(80):
+        deck.append(i)
+
+    table = ['0', '1', '2', '3'],[]
+
+    deck = shuff(deck)
+
+    for i in range(4):
+        for j in range(20):
+            table[i].append(deck.pop())
 
 
 if __name__ == "__main__":
--- MedianAssembly.py
+++ MedianAssembly.py
@@ -2,15 +2,15 @@ __author__ = 'blarson2'
 
 # !/usr/local/bin/python
 
-import tkinter
+from tkinter import *
 
 
 def main():
-    main_window = tkinter.Tk()
-    label = tkinter.Label(main_window, text='Hello World')
+    window = tkinter.Tk()
 
-    label.pack(side='left')
-    tkinter.mainloop()
+    label1 = tkinter.Label(window, text='*angry moth noises*')
+
+    frame1 = Frame()
 
 if __name__ == "__main__":
     main()
